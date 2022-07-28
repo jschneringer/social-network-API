710e3fbece1d611400797a0ae2620618e36bea06
Jordan Schneringer <schneringerj@gmail.com>
2022-07-27 18:44:05 -0700

    updated thought models, added node packages

---
+++ models/thoughts.js
@@ -0,0 +1,70 @@
+const { Schema, model, Types } = require("mongoose");
+const moment = require("moment");
+
+//Reaction Schema
+const reactionsSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxLength: 280,
+    },
+    userName: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) =>
+        moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a"),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const thoughtSchema = new Schema (
+    {
+        thoughtText: {
+            type: String,
+            required: true, 
+            maxlength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => dateFormat(createdAtVal),
+        },
+        userName: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionsSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+
+        },
+        id: false,
+    }
+);
+
+//gets all reactions
+thoughtSchema.virtual("reactionsCount").get(function(){
+    return this.reactions.length;
+
+});
+
+const Thought = model ('Thought', thoughtSchema);
+
+module.exports = Thought;
